f757b42a26152918065bba7625e30ec01252905c
davidfq <davidfq@gmail.com>
2024-11-19 10:47:18 +0100

    Implement application logic to test DB
    List all users, find "John Doe":
    - if present print their username
    - create user if not found

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,18 @@
+import model.DBConnection;
+import model.dao.UserDAO;
+import model.dao.impl.UserDAOImpl;
+import model.entities.User;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        UserDAO userDAO = new UserDAOImpl(DBConnection.getInstance().getConnection());
+
+        userDAO.list().stream()
+            .filter(user -> "john_doe".equals(user.getUsername()))
+            .findFirst()
+            .ifPresentOrElse(
+                user -> System.out.println("User found: " + user.getUsername()),
+                () -> userDAO.create(new User("john_doe", "john@acme.com", "password123")));
+
     }
 }
