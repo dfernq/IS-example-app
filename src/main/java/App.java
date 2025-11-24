0717af11f8eb7a747cc6103d15ab9959a0f0bc57
davidfq <davidfq@gmail.com>
2025-11-24 11:05:11 +0100

    Wire up everything

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,22 +1,36 @@
+import controllers.LoginController;
+import controllers.TodoController;
 import model.DBConnection;
-import model.dao.UserDAO;
+import model.dao.impl.TaskDAOImpl;
 import model.dao.impl.UserDAOImpl;
 import model.entities.User;
+import services.AuthService;
+import services.TaskService;
+import views.LoginView;
+import views.TodoView;
+
+import javax.swing.SwingUtilities;
+import java.sql.Connection;
 
 public class App {
     public static void main(String[] args) {
-        UserDAO userDAO = new UserDAOImpl(DBConnection.getInstance().getConnection());
+        DBConnection dbConnection = DBConnection.getInstance();
+        Connection connection = dbConnection.getConnection();
+        TaskDAOImpl taskDAO = new TaskDAOImpl(connection);
+        UserDAOImpl userDAO = new UserDAOImpl(connection);
+        TaskService taskService = new TaskService(taskDAO);
+        AuthService authService = new AuthService(userDAO);
 
-        userDAO.list().stream()
-            .filter(user -> "john_doe".equals(user.getUsername()))
-            .findFirst()
-            .ifPresentOrElse(
-                user -> System.out.println("User found: " + user.getUsername()),
-                () -> userDAO.create(User.builder()
-                        .username("john_doe")
-                        .email("john@acme.com")
-                        .password("password123")
-                        .build()));
+        SwingUtilities.invokeLater(() -> {
+            LoginView loginView = new LoginView();
+            LoginController loginController = new LoginController(authService, loginView, user -> openTodo(taskService, user));
+            loginController.show();
+        });
+    }
 
+    private static void openTodo(TaskService taskService, User user) {
+        TodoView todoView = new TodoView();
+        TodoController todoController = new TodoController(taskService, todoView, user);
+        todoController.show();
     }
 }
